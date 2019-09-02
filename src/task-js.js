961b04b2a43497c6c5879aa772093f3e016da0d4
sergiy khurtik <skhurtik@ukr.net>
2019-09-03 00:06:50 +0300

    fixed task-3

--- src/task-js.js
+++ src/task-js.js
@@ -99,7 +99,7 @@ const getUsersWithEyeColor = (users, color) =>
 console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 const getUsersWithGender = (users, gender) => {
-  return users.filter(user => user.gender === gender);
+  return users.filter(user => user.gender === gender).map(user => user.name);
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
